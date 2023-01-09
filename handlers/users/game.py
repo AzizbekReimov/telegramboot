a94a9d8217b1d01516cbdbc5378e3900f82c6891
AzizbekReimov <120093969+AzizbekReimov@users.noreply.github.com>
2023-01-09 20:18:41 +0500

    Update game.py

--- handlers/users/game.py
+++ handlers/users/game.py
@@ -2,27 +2,39 @@ from aiogram import types
 
 from loader import dp, bot
 
-from keyboard.inline.game_boshlash import gameMenu
+from keyboards.inline.game_boshlash import gameMenu, game_boshlash
 
 id_royxati = []
 
-@dp.message_hendler(commands="game")
+@dp.message_handler(commands="game")
 async def bot_start(message: types.Message):
-	text = (f"<b>O'yini Boshlaysizmi?</b>\n\n",
-				 f"<b>Boshlash uchun</b>: <u>\"Boshlash\"</u> Tugmasini bosing\n",
+	text = (f"<b>O'yini Boshlaysizmi?</b>",
+				 f"<b>Boshlash uchun</b>: <u>Boshlash</u> Tugmasini bosing",
 				 f"<b>O'ynashni xoxlamasangiz</b>: <u>To'xtatish</u> Tugmasini bosing\n",
+				 f"<b>Savol?</b>\n"
 				 f"<b>O'yin qachon boshlanadi</b>: qachonki o'yinga 10ta odam yigiladi oshandan song boshlanadi")
-	await message.answer(text, reply_markup=gameMenu)
+	await message.answer("\n".join(text), reply_markup=gameMenu)
 
-@dp.message_handler()
-async def bot_start(message: types.Message):
+@dp.callback_query_handler(game_boshlash.filter(item_name="boshlash"))
+async def bot_start(call: types.CallbackQuery):
 	if len(id_royxati)<10:
-		if message.from_user.id not in id_royxati:
-			id_royxati.append(message.from_user.id)
+		if call.from_user.id not in id_royxati:
+			id_royxati.append(call.from_user.id)
 			id_royxat = id_royxati[:]
-			id_royxat.remove(message.from_user.id)
+			id_royxat.remove(call.from_user.id)
 			for id in id_royxat:
 				await bot.send_message(chat_id=id, text=f"Ойинга 1та одам кошилди.\n <b>Жами Ойнчи сони</b>: {len(id_royxati)} ta")
-			await message.answer(f"siz oynga qoshildingiz")
+			await bot.send_message(chat_id=call.from_user.id, text=f"siz oynga qoshildingiz")
 		else:
-			await message.answer("siz oyinga qoshilgansiz")
+			await bot.send_message(chat_id=call.from_user.id, text=f"siz oyinga qoshilgansiz")	
+	await call.answer(cache_time=60)
+	await call.message.edit_reply_markup()
+
+@dp.callback_query_handler(game_boshlash.filter(item_name="toxtatish"))
+async def bot_start(call: types.CallbackQuery):
+	if call.from_user.id not in id_royxati:
+		await bot.send_message(chat_id=call.from_user.id, text=f"O'yin bekor qilindi")
+	else:
+		await bot.send_message(chat_id=call.from_user.id, text=f"Siz o'yinga qoshilib bolgansiz!. \n O'yindan chiqishni xoxlasangiz /cancel commandasini bosing")
+	await call.answer(cache_time=60)
+	await call.message.edit_reply_markup()
