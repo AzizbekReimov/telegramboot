7adc4d29db2f081c1a34fba052515a74f9a41760
AzizbekReimov <120093969+AzizbekReimov@users.noreply.github.com>
2023-01-12 15:44:24 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -8,3 +8,7 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
+
+servers = {}
+server_keys = {}
+server_namee = []
