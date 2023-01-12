ae592a0efee18c01ba61770850c92a0fe7c0bea8
AzizbekReimov <120093969+AzizbekReimov@users.noreply.github.com>
2023-01-12 15:40:47 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,16 +1,17 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from datetime import datetime as dt
+from loader import dp
 
-from loader import dp, bot
 
-id_royxati = [1, 2, 3, 4, 5, 6, 7, 8]
 @dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
-	await message.answer(f"Salom {message.from_user.full_name}!")
-	if len(id_royxati)<10:
-		await message.answer("oyin boshlanib bolgan")
-		if message.from_user.id not in id_royxati:
-			id_royxati.append(message.from_user.id)
-			#for id_royxat in id_royxati:
-				#await bot.send_message(chat_id=id_royxat, text=f"oyinga 1ta odam qo'shild")
-	await message.answer("oyin boshlanib bolgan")
+async def bot_start(msg: types.Message):
+    text = f"Ассалому алейкум <b>{msg.from_user.full_name}</b>\n\n"
+    text += f"<b>Бот хакида кискача малумот:</b>\n"
+    text += f"Ушбу ботда сиз достларингиз билан сервер очиб содда ойин ойнашингиз мумкин🎮\n\n"
+    text += f"<b>камандалар:</b>\n"
+    text += f"Сервер яратиш учун /creat_server камандасини босинг.\n"
+    text += f"Ёки брорта серверда ойнамокчи булсангиз /get_server камандасини босинг.\n\n   "
+    text += f"<b>Мурожат учун:</b>\n"
+    text += f"Таклф Ёки саволлар булса куйидаги админ блан богдланинг @azizbek_reimov\n"
+    await msg.answer(f"{text}")
