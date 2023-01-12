2a3c6ff6af6a297d8a492892f8dccab3a1404f4e
AzizbekReimov <120093969+AzizbekReimov@users.noreply.github.com>
2023-01-12 16:58:15 +0500

    Update get_server.py

--- handlers/users/get_server.py
+++ handlers/users/get_server.py
@@ -1,6 +1,5 @@
 from aiogram import types
 from typing import Union
-
 from loader import dp, bot
 
 from data.config import servers, server_keys
@@ -8,11 +7,9 @@ from keyboards.inline.get_servers import Servers, Server, menu_cd, add_serverr
 from .creat_server import server_menu
 import time
 
-delete = {}
 servername = []
 @dp.message_handler(commands="get_server")
 async def show_menu(message: types.Message):
-    delete[message.from_user.id]=[]
     await list_categories(message)
 
 add_server_xaqida = []
@@ -33,14 +30,6 @@ async def list_categories(messagee: Union[types.CallbackQuery, types.Message], *
             await call.message.edit_text(text=f"<b>~~~~~~~~~~~~~~~~~~~~~~~~~~</b>\n<b>Серверлар ройхати</b>\n<b>Мавжуд серверлар сони</b> {len(servers)}\n\
     <b>~~~~~~~~~~~~~~~~~~~~~~~~~~</b>", reply_markup=markup)
     else:
-        while True:
-            try:
-                print(delete)
-                await bot.delete_message(chat_id=messagee.from_user.id, message_id=delete[messagee.from_user.id][-1])
-                del delete[messagee.from_user.id][-1]
-            except IndexError:
-                print("xato")
-                break
         await add_server(messagee=messagee)
     
 async def show_item(callback: types.CallbackQuery, item_id):
@@ -53,23 +42,18 @@ async def show_item(callback: types.CallbackQuery, item_id):
 
 @dp.callback_query_handler(menu_cd.filter(item_id="add_server"))
 async def add_server(messagee: Union[types.CallbackQuery, types.Message]):
-    marcup = Cancel_Server()
     try:
         players = add_server_xaqida[-1]['players']
     except IndexError:
         call = messagee
-        n3 = await call.answer(f"Siz oldindan serverga ulangansiz serverdan chiqish uchun /cancel_server commandasiga murojat qiling")
-        delete[messagee.from_user.id].append(n3.message_id)
+        await call.answer(f"Siz oldindan serverga ulangansiz serverdan chiqish uchun /cancel_server commandasiga murojat qiling")
     else:
         if isinstance(messagee, types.CallbackQuery):
             players.append(messagee.from_user.id)
-            n1 = await messagee.message.edit_text(text=players, reply_markup=marcup)
-            delete[messagee.from_user.id].append(n1.message_id)
+            await messagee.message.edit_text(text=f"Siz serverga qoshildingiz")
         elif messagee:
-            n2 = await messagee.answer(players, reply_markup=marcup)
+            await messagee.answer(f"Siz oldindan serverga ulangansiz serverdan chiqish uchun /cancel_server commandasiga murojat qiling")
             del add_server_xaqida[-1]
-            delete[messagee.from_user.id].append(n2.message_id)
-    print(delete)
             
 
 @dp.callback_query_handler(menu_cd.filter())
